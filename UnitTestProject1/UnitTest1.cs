0f858706c01ff53544834d5480fdb03535e79186
jonathan-r-jones <raybass8@gmail.com>
2016-01-30 10:10:33 -0500

    ecc first commit

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int test = 1;
+            Assert.AreEqual(1, test);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            int test = 3;
+            Assert.AreEqual(2, test);
+        }
+
+        public void PrintString(int numberOfTimeToPrint)
+        {
+            for (int i = 1; i <= numberOfTimeToPrint; i++)
+            {
+                Console.WriteLine("Hello World");
+            }
+        }
+
+        public void TestPrintString()
+        {
+            PrintString(3);
+        }
+
+    }
+}
